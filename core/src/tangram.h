664340d0120f3b3cea9569fdf3d692ca4742d6f9
Varun Talwar <tallytalwar@gmail.com>
2014-11-03 11:25:41 -0500

    Added calls to input handling to the uber tangram control. handleTouchEvents: reads touchEvents from platform specific code and passes to the input manager for gesture detection applyGesture: calls from platform's update call, to apply already detected gestures clearGesture: calls from platform's update call to clear all gesture states after these have been applied.

--- core/src/tangram.h
+++ core/src/tangram.h
@@ -10,6 +10,7 @@
 #include "util/shaderProgram.h"
 #include "util/vertexLayout.h"
 #include "util/vboMesh.h"
+#include "input/touchEventMgr.h"
 
 /* Tangram C API
  *
@@ -28,3 +29,18 @@ void resizeViewport(int newWidth, int newHeight);
 // Render a new frame of the map view (if needed)
 // TODO: Pass in the time step from the client application
 void renderFrame();
+
+/*
+ * Passes the platform independent TouchEvent to touchEventMgr for processing
+ */
+void handleTouchEvents(const Tangram::TouchEvent& _touchEvent);
+
+/*
+ * Checks against various gestures and applies the corresponding gesture to the application
+ */
+void applyGestures();
+
+/*
+ * Clears the state of all gestures detected in the last update call!
+ */
+void clearGestures();
